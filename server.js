f3eb16445d91da748f04fe84f17a7f0ee2da69e0
Jeremy Price <JeremyPriceM@gmail.com>
2019-02-16 11:31:32 -0600

    Fixed server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(morgan("common"));
 app.use(express.json());
-app.use("/blog-posts", "blogPostsRouter");
+app.use("/blog-posts", blogPostsRouter);
 
 app.listen(process.env.PORT || 8080, () => {
     console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
